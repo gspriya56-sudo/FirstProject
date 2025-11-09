6d78a045e692bf40d3d30c8bc3166516a38ec626
gspriya56-sudo <gspriya56@gmail.com>
2025-11-09 12:24:41 +0530

    commit

---
+++ Source/Main/Nestedif.py
@@ -0,0 +1,18 @@
+def student_grade(marks):
+    if marks >= 90:
+        return "A"
+        
+    elif marks >= 80:
+            return "B"
+           
+    elif marks >= 70:
+                return "C"
+              
+    elif marks >= 60:
+                    return "D"
+                  
+    else: return "F"
+                    
+
+
+print(student_grade(11))
